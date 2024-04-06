3a9ae8d4d4e56ba0aae0caa0495d7f36d12ca79f
anastasiafff <anastasiafebriyanti997@gmail.com>
2024-04-06 16:19:42 +0700

    perbaiki

--- Tugas5/login.php
+++ Tugas5/login.php
@@ -28,7 +28,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
+<head>  
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
